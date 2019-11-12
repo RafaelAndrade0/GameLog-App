0c5444bcf0cfdbd08885be5dd8d19f58c4035ddb
Rafael Andrade Silva <rafael.andrade.dev0@gmail.com>
2019-11-12 10:16:44 -0300

    About & Home Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+import About from './components/pages/About';
+import Home from './components/pages/Home';
 
 const App: React.FC = () => {
 	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route exact path="/about">
+						<About />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
 };
 
