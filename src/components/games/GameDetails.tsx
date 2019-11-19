fbe89fb9d5334ac0bac535c632aca85e3c287d04
Rafael Andrade Silva <rafael.andrade.dev0@gmail.com>
2019-11-19 11:46:06 -0300

    GameDetails Structure

--- src/components/games/GameDetails.tsx
+++ src/components/games/GameDetails.tsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import { RouteComponentProps } from 'react-router';
-import { Segment } from 'semantic-ui-react';
+import { Segment, Item, Label, Container, Image, Button, Icon } from 'semantic-ui-react';
 import GamesApi from '../../api/agent';
+import { IGame } from '../../models/game';
+import { IResult } from '../../models/result';
+import { Link } from 'react-router-dom';
 
 interface Iprops {
 	id: string;
@@ -12,15 +15,53 @@ const GameDetails: React.FC<RouteComponentProps<Iprops>> = ({ match }) => {
 		getGame();
 	}, []);
 
+	const [ singleGame, setSingleGame ] = useState<IGame>({
+		genre: '',
+		description: '',
+		developer: [],
+		initialrelease: '',
+		plataform: '',
+		title: '',
+		id: '',
+		photo: ''
+	});
+
 	const getGame = async () => {
-		const result = await GamesApi.getGame(match.params.id);
-		console.log(result);
+		const result: IResult<IGame> = await GamesApi.getGame(match.params.id);
+		setSingleGame(result.data);
 	};
 
 	return (
-		<Segment>
-			<h1>{match.params.id}</h1>
-		</Segment>
+		<Fragment>
+			<Link to='/'>
+				<Button animated color='orange'>
+					<Button.Content visible>Go Back!</Button.Content>
+					<Button.Content hidden>
+						<Icon name='arrow left' />
+					</Button.Content>
+				</Button>
+			</Link>
+
+			<Segment clearing>
+				<Container>
+					<Item>
+						<Item.Image
+							bordered
+							size='small'
+							floated='left'
+							src={`http://localhost:5000/uploads/${singleGame.photo}`}
+						/>
+						<Item.Content>
+							<Item.Header as='h2'>{singleGame.title}</Item.Header>
+							<Item.Description>{singleGame.description}</Item.Description>
+							<Item.Extra>
+								<Label>{singleGame.plataform}</Label>
+							</Item.Extra>
+						</Item.Content>
+					</Item>
+				</Container>
+			</Segment>
+		</Fragment>
 	);
 };
 
