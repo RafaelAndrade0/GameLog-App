cfca66fa8a9334b4f1a5df2c9b4b12281c298638
Rafael Andrade Silva <rafael.andrade.dev0@gmail.com>
2019-11-28 13:42:57 -0300

    TextInput Form

--- src/components/auth/LoginComponent.tsx
+++ src/components/auth/LoginComponent.tsx
@@ -4,46 +4,50 @@ import { observer } from 'mobx-react-lite';
 import { RootStoreContext } from '../../stores/rootStore';
 import { IUserFormValues } from '../../models/user';
 
+import { Form as FinalForm, Field } from 'react-final-form';
+import TextInput from '../../commom/form/TextInput';
+
 const LoginComponent: React.FC = () => {
 	const rootStore = useContext(RootStoreContext);
 	const { login, loading } = rootStore.userStore;
-	const [ userForm, setUserForm ] = useState<IUserFormValues>({ email: '', password: '' });
 
-	const onFormSubmit = () => {
-		login(userForm);
-	};
-
-	const handleInputChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-		const { name, value } = event.currentTarget;
-		setUserForm({ ...userForm, [name]: value });
+	const handleFinalFormSubmit = (formValue: IUserFormValues) => {
+		login(formValue);
 	};
 
 	return (
 		<Grid className='login' textAlign='center' verticalAlign='middle'>
 			<Grid.Column>
-				<Form size='large' onSubmit={onFormSubmit}>
-					<Form.Input
-						onChange={handleInputChange}
-						fluid
-						icon='mail'
-						iconPosition='left'
-						placeholder='E-mail address'
-						name='email'
-					/>
-					<Form.Input
-						onChange={handleInputChange}
-						fluid
-						icon='lock'
-						iconPosition='left'
-						placeholder='Password'
-						type='password'
-						name='password'
-					/>
+				<FinalForm
+					onSubmit={handleFinalFormSubmit}
+					render={({ handleSubmit }) => (
+						<Form size='large' onSubmit={handleSubmit}>
+							<Field
+								// onChange={handleInputChange}
+								// fluid
+								// icon='mail'
+								// iconPosition='left'
+								placeholder='E-mail address'
+								name='email'
+								component={TextInput}
+							/>
+							<Field
+								// onChange={handleInputChange}
+								// fluid
+								// icon='lock'
+								// iconPosition='left'
+								placeholder='Password'
+								type='password'
+								name='password'
+								component={TextInput}
+							/>
 
-					<Button color='blue' fluid size='large' loading={loading}>
-						Login
-					</Button>
-				</Form>
+							<Button color='blue' fluid size='large' loading={loading}>
+								Login
+							</Button>
+						</Form>
+					)}
+				/>
 			</Grid.Column>
 		</Grid>
 	);
